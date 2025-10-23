f3ea98d67edfa712f70bc4eba22e183d4fcde185
Bad-Dot-Ice-Cream <testingforspace@gmail.com>
2025-10-23 08:40:46 -0400

    Add files via upload

--- Build-a-Dino-Expanded.py
+++ Build-a-Dino-Expanded.py
@@ -7,5 +7,5 @@ class Dinosaur:
     def __init__(self, name, species, diet, age):
         self.name = name
         self.species = species  #  [Flying Dinosaur, Water Dinosaur] etc.
-        self.diet = diet        #  ["Leafy greens", "Fresh meat", "Fish", "Carrion"]  General range of dietary classifications
+        self.diet = diet
         self.age = age
